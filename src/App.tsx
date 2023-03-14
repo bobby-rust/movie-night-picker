828b8b0430907c865ffc3fd6aa7c00e9e162eeda
Bobby <bobby.rust121@gmail.com>
2023-03-14 12:53:08 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import config from "./config.json";
+
+enum SortOptions {
+    Popularity = "popularity.desc",
+    Rating = "vote_average.desc",
+    Latest = "release_date.desc",
+    Revenue = "revenue.desc",
+    VoteCount = "vote_count.desc",
+}
+
+enum Genre {
+    Action = "Action",
+    Adventure = "Adventure",
+    Animation = "Animation",
+    Comedy = "Comedy",
+    Crime = "Crime",
+    Documentary = "Documentary",
+    Drama = "Drama",
+    Family = "Family",
+    Fantasy = "Fantasy",
+    History = "History",
+    Horror = "Horror",
+    Music = "Music",
+    Mystery = "Mystery",
+    Romance = "Romance",
+    ScienceFiction = "Science Fiction",
+    TVMovie = "TV Movie",
+    Thriller = "Thriller",
+    War = "War",
+    Western = "Western",
+}
+
+/**
+ * Add cast filter in future, need the actor's IDs
+ */
+type FilterOptions = {
+    genre: Genre;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Set up state
+    const [page, setPage] = useState<number>(1);
+    const [sortOptions, setSortOptions] = useState<SortOptions>(
+        SortOptions.Popularity
+    );
+
+    const handlePageForward = () => {
+        setPage(page + 1);
+    };
+    const handlePageBackward = () => {
+        setPage(page - 1);
+    };
+
+    return (
+        <div className='App'>
+            <button onClick={handlePageForward} />
+            <button onClick={handlePageBackward} />
+        </div>
+    );
 }
 
 export default App;
